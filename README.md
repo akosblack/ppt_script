40aceaacf30c24a63dfd979cb7bcd5c7161dea05
Fekete Akos <fekete.akos@t-systems.hu>
2023-09-21 09:19:31 +0200

    Updated
    Updated:
    - now the input file look likes this:
    [keywords] | [replacements]
    so it can replace the defined keywords in the pptx file.
    - Added timestamp to the new file name.
    - Formatted the README file a bit more

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ A script that replace placeholders in a ppt* file form a xls* file.
 
 How its working:
 
-  From the input directory checking if there is any xls and a ppt file available
+  - From the input directory checking if there is any xls and a ppt file available
   
-  From the xlx file its gets the data (one row is one data)
+  - From the xlx file its gets the data (one row is one data)
   
-  Then replace all defined keywords to the data (the first keyword to the first data)
+  - Then replace all defined keywords to the data (the first keyword to the first data)
--- script.py
+++ script.py
@@ -2,13 +2,16 @@ import os
 import shutil
 from pptx import Presentation
 import openpyxl
+from datetime import datetime
 
-def replace_text_in_ppt(pptx_file,replacements):
-    keywords = ["<name>", "<age>", "<city>"]
+def replace_text_in_ppt(pptx_file, keywords, replacements):
     # Másolja az eredeti fájlt az "output" mappába
     output_folder = "output"
     os.makedirs(output_folder, exist_ok=True)
-    pptx_file_output = os.path.join(output_folder, "pptx_new_file.pptx")
+    time = datetime.now().strftime("%Y-%m-%d")
+    pptx_file_output = os.path.join(output_folder, pptx_file.replace('input\\',''))
+    pptx_file_output = pptx_file_output.replace('.pptx', f'_{time}.pptx')
+    print(pptx_file_output)
     shutil.copy(pptx_file, pptx_file_output)
     # Nyissa meg a másolt fájlt
     prs = Presentation(pptx_file_output)
@@ -20,30 +23,30 @@ def replace_text_in_ppt(pptx_file,replacements):
     prs.save(pptx_file_output)
     return pptx_file_output
 
-def read_replaceable_input(replaceable_input):
+def read_input(excel_input):
+    keywords = []
     replacements = []
     # Nyissa meg az xlsx fájlt
-    workbook = openpyxl.load_workbook(replaceable_input)
+    workbook = openpyxl.load_workbook(excel_input)
     worksheet = workbook.active
     # Olvassa be a tartalmat soronként
     for row in worksheet.iter_rows(values_only=True):
-        for cell_value in row:
-            replacements.append(str(cell_value))
-    return replacements
-
+        keyword, replacement = row
+        keywords.append(str(keyword))
+        replacements.append(str(replacement))
+    return keywords, replacements
 
 if __name__ == "__main__":
     try:      
         input_folder = "input"
         os.makedirs(input_folder, exist_ok=True)  
         # Keresse meg az input mappában található .xls vagy .xlsx fájlt
-        replaceable_input = None
+        excel_input = None
         for filename in os.listdir(input_folder):
             if filename.endswith((".xls", ".xlsx")):
-                replaceable_input = os.path.join(input_folder, filename)
+                excel_input = os.path.join(input_folder, filename)
                 break
-
-        if replaceable_input is None:
+        if excel_input is None:
             raise Exception("Nem található .xls vagy .xlsx fájl az input mappában.")
 
         # Keresse meg az input mappában található .ppt vagy .pptx fájlt
@@ -52,15 +55,14 @@ if __name__ == "__main__":
             if filename.endswith((".ppt", ".pptx")):
                 template_pptx = os.path.join(input_folder, filename)
                 break
-
         if template_pptx is None:
             raise Exception("Nem található .ppt vagy .pptx fájl az input mappában.")
 
-        replacements = read_replaceable_input(replaceable_input)
+        keywords, replacements = read_input(excel_input)
+        print(keywords)
         print(replacements)
-        new_pptx_file = replace_text_in_ppt(template_pptx, replacements)
+        new_pptx_file = replace_text_in_ppt(template_pptx, keywords, replacements)
         os.system(f"start {new_pptx_file}")
-
         # Kiírja, hogy végzett
         print("Az adatok kicserelése befejezve. Nyomj meg egy billentyűt a kilépéshez...")
         input()
