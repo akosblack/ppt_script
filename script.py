4064f7ad98f9748206afa78222e158ed58bf234a
Fekete Akos <fekete.akos@t-systems.hu>
2023-09-27 13:08:09 +0200

    Charts inprogress

--- script.py
+++ script.py
@@ -17,7 +17,7 @@ def get_latest_version():
     version_data = requests.get("https://raw.githubusercontent.com/akosblack/ppt_script/main/latest_version.txt").text
     return str(version_data)
 
-def replace_text_in_ppt(pptx_file, keywords, replacements):
+def replace_text_in_ppt(pptx_file, keywords, replacements, charts):
     print("Az adatok kicserélése folyamatban...")
     # Másolja az eredeti fájlt az "output" mappába
     output_folder = "output"
@@ -33,41 +33,67 @@ def replace_text_in_ppt(pptx_file, keywords, replacements):
         for shape in slide.shapes:
             if shape.has_chart:
                     chart = shape.chart
-                    update_chart(chart)
+                    update_chart(chart, charts[0])
+                    if len(charts) > 0:                        
+                        charts.remove(charts[0])
+                        print(f'charts: {charts}')
             if hasattr(shape, "text"):
                 for keyword, replacement in zip(keywords, replacements):
                     shape.text = shape.text.replace(keyword, replacement)
     prs.save(pptx_file_output)
     return pptx_file_output
 
-def update_chart(chart):
+def update_chart(chart, chart_data):
     print(chart.chart_type)
-    chart.chart_title.text_frame.text = 'asd'
-
-    chart_data = ChartData()
-    chart_data.categories = 'Foobar', 'Barbaz', 'Bazfoo'
-    chart_data.add_series('New Series 2', (2.3, 3.4, 4.5))
-    chart_data.add_series('New Series 3', (8.9, 9.1, 1.2))
 
     chart.replace_data(chart_data)
 
 def read_input(excel_input):
     keywords = []
     replacements = []
+    chart_keywords = []
     chart_datas = []
+    charts = []
     # Nyissa meg az xlsx fájlt
     workbook = openpyxl.load_workbook(excel_input)
     worksheet = workbook.active
+    current_chart_type = None
     # Olvassa be a tartalmat soronként
     for row in worksheet.iter_rows(values_only=True):
-        if 'kat' in row[0]:
-            kat, chart_data = row
-            chart_datas.append(chart_data)
-        else:
-            keyword, replacement = row        
-            keywords.append(str(keyword))
-            replacements.append(str(replacement))
-    return keywords, replacements, chart_datas
+        if row[0] is not None:
+            cell_value = str(row[0]).strip().lower()
+            if any(cell_value.startswith(chart_type) for chart_type in ["column", "line", "pie", "bar", "area"]):
+                print(f'cell_value in current chart type: {cell_value}')
+                current_chart_type = cell_value
+                chart_data = ChartData()
+                if current_chart_type == 'column':
+                    print(f'found chart data in excel which is column')
+                    if 'title' in cell_value:
+                        print(f'title: {row[1]}')
+                        chart_data.chart.title.text_frame.text = row[1]
+                    elif 'categories' in cell_value:
+                        print(f'categories: {row[1:]}')
+                        chart_data.categories = row[1:]
+
+                    elif 'series' in cell_value:
+                        print(f'series: {row[0]} {row[1]}')
+                        chart_data.add_series(row[0], row[1])
+
+                if current_chart_type is not None:
+                    charts.append(chart_data)
+                    #print(f'charts: {charts}')                
+
+                chart_keyword, chart_data2 = row
+                chart_keywords.append(str(chart_keyword))
+                chart_datas.append(str(chart_data2))
+                current_chart_type = None
+                continue
+            else:
+                keyword, replacement = row
+                keywords.append(str(keyword))
+                replacements.append(str(replacement))
+
+    return keywords, replacements, charts
 
 if __name__ == "__main__":
     # print(get_current_version(), get_latest_version())
@@ -96,11 +122,12 @@ if __name__ == "__main__":
         if template_pptx is None:
             raise Exception("Nem található .ppt vagy .pptx fájl az input mappában.")
 
-        keywords, replacements, chart_data = read_input(excel_input)
-        print(f'chart_data: {chart_data}')
-        # print(keywords)
-        # print(replacements)
-        new_pptx_file = replace_text_in_ppt(template_pptx, keywords, replacements)
+        keywords, replacements, charts = read_input(excel_input)
+        # print(f'chart_keywords: {chart_keywords}')
+        # print(f'chart_data: {chart_datas}')
+        print(keywords)
+        print(replacements)
+        new_pptx_file = replace_text_in_ppt(template_pptx, keywords, replacements, charts)
         os.system(f"start {new_pptx_file}")
         # Kiírja, hogy végzett
         print("Az adatok kicserelése befejezve. Nyomj meg egy billentyűt a kilépéshez...")
